d2670b6cb3cf424948226c30e4a91595a542a8df
Etienne Vautherin <etienne@vautherin.com>
2022-02-08 12:03:08 +0100

    Initial Commit

---
+++ Tuesday/TuesdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuesdayApp.swift
+//  Tuesday
+//
+//  Created by Etienne Vautherin on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct TuesdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
